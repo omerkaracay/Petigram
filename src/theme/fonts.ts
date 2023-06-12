ef168fb95b9e6418e1fb0bf8a9199e211865344b
Ömer Karaçay <omerkaracay@gmail.com>
2023-06-13 01:00:35 +0300

    added textstyle sets for app theme

---
+++ src/theme/fonts.ts
@@ -0,0 +1,19 @@
+const size = {
+  xs: 10,
+  sm: 12,
+  default: 14,
+  md: 16,
+  lg: 20,
+  xl: 24,
+  xxl: 30,
+};
+
+const weight = {
+  full: '900',
+  bold: 'bold',
+  semibold: '600',
+  normal: 'normal',
+  thin: '400',
+};
+
+export default {size, weight};
