956b3ba8d319c867fc57b34bc318607f30425373
Ömer Karaçay <omerkaracay@gmail.com>
2023-06-13 01:18:39 +0300

    added vector-icons package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Petigram</Text>
+      <Text
+        style={{
+          color: colors.primary,
+          fontSize: fonts.size.xxl,
+        }}>
+        Petigram
+        <AntDesign name="stepforward" size={25} />
+      </Text>
     </View>
   );
 }
